9e96ee052137f4b15622924a3286b5d2707b4276
Luis Almaguer <imaximuz@gmail.com>
2020-02-02 10:29:43 -0600

    TrendingDisplay

---
+++ Assets/Scripts/TrendingImageSelector.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrendingImageSelector : MonoBehaviour
+{
+
+    public Sprite[] spritePool;
+    Image currentImage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentImage = GetComponent<Image>();
+        if(currentImage == null)
+            Debug.Log("Image component not found");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TrendChange(PotType type) {
+        int index = (int)type;
+        Debug.Log(index);
+        if(index >= spritePool.Length)
+            return;
+        if(currentImage != null)
+            currentImage.sprite = spritePool[index];
+    }
+}

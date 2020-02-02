4273c3b84bd6f2f292e1d057ef2960f33cf476c8
Luis Almaguer <imaximuz@gmail.com>
2020-02-02 01:43:40 -0600

    Billboard tendencias

---
+++ Assets/Scripts/LookAtCamera.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    public Camera target_camera;
+
+    void Update()
+    {
+        if(target_camera != null) {
+            transform.LookAt(transform.position + target_camera.transform.rotation * Vector3.back, 
+                target_camera.transform.rotation * Vector3.up);
+        }
+    }
+}
--- Assets/Scripts/testMovement.cs
+++ Assets/Scripts/testMovement.cs
@@ -6,16 +6,21 @@ using UnityEngine;
 public class testMovement : MonoBehaviour
 {
     float t = 0;
-    // Start is called before the first frame update
-    void Start()
-    {
-    }
+    public bool doRotation = false;
+    public bool doMovement = true;
+    public Vector3 axis = Vector3.up;
 
     // Update is called once per frame
     void Update()
     {
         t += Time.deltaTime;
-        Vector3 newForce = new Vector3((float)Math.Sin(t * 2) * 4 * Time.deltaTime, 0, 0);
-        transform.Translate(newForce);
+        if(doMovement){
+            Vector3 newForce = new Vector3((float)Math.Sin(t * 2) * 4 * Time.deltaTime, 0, 0);
+            transform.Translate(newForce);
+        }
+
+        if(doRotation) {
+            transform.Rotate(axis * 45 * Time.deltaTime);
+        }
     }
 }
